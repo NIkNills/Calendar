f67774a3a279d982d28ba83eeaed97ef37eedcec
Nik Nills <niknills94@gmail.com>
2022-01-19 19:24:51 +0300

    feat: add event to buttons

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const time = document.querySelector('.time'),
       month = document.querySelector('.month'),
-      date = document.querySelector('.date'),
+      todaysDate = document.querySelector('.todays-date'),
       weekDays = document.querySelector('.week-days'),
       monthsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      weekDaysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      weekDaysArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
       days = document.querySelector('.days'),
       btnNext = document.querySelector('.btn-next'),
       btnPrev = document.querySelector('.btn-prev');
 
-let now = new Date();
+let date = new Date();
 
 let clock = document.createElement('h1');
 clock.className = 'clock';
@@ -27,54 +27,69 @@ function showTime() {
   setInterval(showTime, 100);
 }
 
-let thisMonth = monthsArr[now.getMonth()];
+let thisMonth = monthsArr[date.getMonth()];
+let thisYear = date.getFullYear();
 
-let thisYear = now.getFullYear();
+function renderMonthInfo() {
+  month.innerHTML = `<p>${monthsArr[date.getMonth()]} ${date.getFullYear()}</p>`
+}
 
-let monthInfo = document.createElement('p');
-monthInfo.innerHTML = `${thisMonth} ${thisYear}`;
-month.append(monthInfo);
-
-let thisWeekDay = weekDaysArr[now.getDay()];
-let thisDate = now.getDate();
+let thisWeekDay = weekDaysArr[date.getDay() - 1];
+let thisDate = date.getDate();
 
 let dateInfo = document.createElement('p');
-dateInfo.innerHTML = `${thisWeekDay}, ${thisDate} ${thisMonth} ${thisYear}`;
-date.append(dateInfo);
+dateInfo.innerHTML = `${thisWeekDay}, ${thisDate} ${thisMonth} ${date.getFullYear()}`;
+todaysDate.append(dateInfo);
 
 for(let i = 0; i < weekDaysArr.length; i++) {
   weekDays.insertAdjacentHTML('beforeend', `<div class="week-day">${weekDaysArr[i].slice(0, 3)}</div>`)
 }
-
-let selectedMonth = now.getMonth();
 
 function getLastDayOfMonth(thisYear, selectedMonth) {
   let date = new Date(thisYear, selectedMonth + 1, 0);
   return date.getDate();
 }
 
-now.setDate(1);
+function renderCalendar () {
+  date.setDate(1);
 
-const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-const prevLastDay = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
-const firstDayIndex = now.getDay();
-const lastDayIndex = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDay();
-const nextDays = 7 - lastDayIndex - 1;
-
-for(let x = firstDayIndex; x > 0; x--) {
-  days.insertAdjacentHTML('beforeend', `<div class="day day__prev">${prevLastDay - x + 1}</div>`);
-}
-
-for(let day = 1; day <= lastDay; day++) {
-  if(day == thisDate) {
-    days.insertAdjacentHTML('beforeend', `<div class="day day__active">${day}</div>`);
-  } else {
-    days.insertAdjacentHTML('beforeend', `<div class="day">${day}</div>`);
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+  const firstDayIndex = date.getDay() - 1;
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+  const nextDays = 7 - lastDayIndex;
+  
+  for(let x = firstDayIndex; x > 0; x--) {
+    days.innerHTML += `<div class="day day__prev">${prevLastDay - x + 1}</div>`;
+  }
+  
+  for(let i = 1; i <= lastDay; i++) {
+    if(i === thisDate && thisMonth === monthsArr[date.getMonth()] && thisYear === date.getFullYear()) {
+      days.innerHTML += `<div class="day day__active">${i}</div>`;
+    } else {
+      days.innerHTML += `<div class="day">${i}</div>`;
+    }
+  }
+  
+  for(let j = 1; j <= nextDays; j++) {
+    days.innerHTML += `<div class="day day__next">${j}</div>`;
   }
 }
 
-for(let j = 1; j <= nextDays; j++) {
-  days.insertAdjacentHTML('beforeend', `<div class="day day__next">${j}</div>`);
-}
+btnPrev.addEventListener("click", () => {
+  days.innerHTML = "";
+  date.setMonth(date.getMonth() - 1);
+  renderMonthInfo();
+  renderCalendar();
+})
+
+btnNext.addEventListener("click", () => {
+  days.innerHTML = "";
+  date.setMonth(date.getMonth() + 1);
+  renderMonthInfo();
+  renderCalendar();
+})
 
 showTime();
+renderCalendar();
+renderMonthInfo();
